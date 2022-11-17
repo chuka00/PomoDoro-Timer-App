81abdb8c2353323352d9912be52585c375a422b8
Victor Chukwuka Ogbodo <chukaogbodo7@gmail.com>
2022-11-17 12:27:13 +0100

    .

--- PomoDoro Timer App/Program.cs
+++ PomoDoro Timer App/Program.cs
@@ -4,13 +4,19 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kindly input your prefered mode \n 'S' for Seconds or 'M' for Minutes");
+            var input = Console.ReadLine().ToUpper();
             Console.WriteLine("Kindly enter your desired worktime");
             var workTime = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Kindly enter your desired rest period");
             var restTime = Convert.ToInt32(Console.ReadLine());
 
             while (true)
-            { 
+            {
+
+
+                if (input == "S")
+                {
 
                     for (var i = workTime - 1; i <= workTime; i--)
                     {
@@ -18,7 +24,7 @@
                         Console.Write("\rWorkTime: " + i);
                         if (i == 0)
                         {
-                            Console.Write("Time to get some rest\n");
+                            Console.Write("\rTime to get some rest");
                             restCountDown(restTime);
                         }
                     }
@@ -37,13 +43,12 @@
                             }
                         }
                     }
-                
-                
-                
-                    //var convertedWorkTime = Convert.ToInt32(workTime);
-                    /*var restTime = Console.ReadLine();*/
-                    /*var convertedRestTime = Convert.ToInt32(restTime);*/
-                    //Console.CursorVisible = false;
+
+
+                }
+                else if (input == "M")
+                {
+
                     var minuteTimer = new DateTime(2000, 1, 1, 0, workTime, 0);
 
                     for (int i = 0; i <= workTime * 60; i++)
@@ -52,22 +57,19 @@
                         minuteTimer = minuteTimer.AddSeconds(-1);
                         Thread.Sleep(1000);
                         Console.Clear();
-                        restCountDown();
                     }
 
-                    static void restCountDown(int restTime)
-                    {
-                        var minuteTimer = new DateTime(2000, 1, 1, 0, restTime, 0);
 
-                        for (int i = 0; i <= restTime * 60; i++)
-                        {
-                            Console.Write(minuteTimer.ToString("mm:ss"));
-                            minuteTimer = minuteTimer.AddSeconds(-1);
-                            Thread.Sleep(1000);
-                            Console.Clear();
-                        }
+                    for (int i = 0; i <= restTime * 60; i++)
+                    {
+                        Console.Write(minuteTimer.ToString("mm:ss"));
+                        minuteTimer = minuteTimer.AddSeconds(-1);
+                        Thread.Sleep(1000);
+                        Console.Clear();
                     }
                 }
             }
+        }
+            
     }
 }
